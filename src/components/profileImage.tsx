2492cde459b92498fd567b355bcbd70b6aa98153
kiramDZ <bukutulis0110@gmail.com>
2024-08-04 15:30:10 +0700

    :lipstick: update profile

--- src/components/profileImage.tsx
+++ src/components/profileImage.tsx
@@ -1,18 +1,27 @@
 import Image from "next/image";
-import Profile from "../../public/assets/profile.png";
+// import Profile from "../../public/assets/profile.png";
+import NotionProfile from "../../public/assets/notion-profile-border.png";
+import NotionProfileDark from "../../public/assets/notion-profile-white.png";
+import { useTheme } from "next-themes";
 
-const BroKariim = () => (
+interface Props {
+  theme?: string;
+}
+
+const BroKariim = ({ theme }: Props) => (
   <Image
     className="h-48 w-48 rounded-full dark:border-gray-800  my-4"
-    src={Profile}
+    // src={NotionProfile}
+    src={theme === "dark" ? NotionProfileDark : NotionProfile}
     alt=""
   />
 );
 
 export const ProfileImage = () => {
+  const { theme } = useTheme();
   return (
     <div className="flex justify-start items-start w-full">
-      <BroKariim />
+      <BroKariim theme={theme} />
     </div>
   );
 };
